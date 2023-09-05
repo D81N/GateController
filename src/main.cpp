f185aace126e677e0871fc1dd3ce7450a2544edb
Volodymyr Hut <volodymyr.hut@gmail.com>
2023-09-05 14:42:17 +0300

    prepared for testing "/open-gate" endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
-#include <Arduino.h>
+#include "network/Network.h"
+#include "gate_server/GateServer.h"
+
+GateLeaf    left(D0, D1);
+GateLeaf    right(D2, D3);
+Gate        gate(left, right, 1000);
+GateServer  server(80, gate);
 
 void setup() {
-
+    gate.initPins();
+    Network::connectToWifi("TP-LINK_EFDA52", "kul1551luk");
+    server.start();
 }
 
 void loop() {
-
+    server.handleClient();
 }
