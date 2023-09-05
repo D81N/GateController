ef41fed0f32d6337a26df034a72f4d0a8ff20cb9
Volodymyr Hut <volodymyr.hut@gmail.com>
2023-09-05 14:41:15 +0300

    added one more check to "/open-gate" endpoint

--- src/gate_server/GateServer.cpp
+++ src/gate_server/GateServer.cpp
@@ -5,7 +5,7 @@ GateServer::GateServer(int32_t port, Gate &gate) :
         gate(gate) {}
 
 void GateServer::handleOpenGate() {
-    server.on("/openGate", HTTP_POST, [&]() {
+    server.on("/open-gate", HTTP_POST, [&]() {
 
         if (gate.errorIsPresent()) {
             server.send(503,
@@ -22,9 +22,15 @@ void GateServer::handleOpenGate() {
             return;
         }
 
+        if (gate.getState() == State::OPENED) {
+            server.send(423,
+                        "application/json",
+                        R"({"message":"Can`t open gate. It`s already opened"})");
+            return;
+        }
+
         gate.open();
         server.send(200, "application/json", R"({"message":"Gate is opening"})");
-
     });
 }
 
