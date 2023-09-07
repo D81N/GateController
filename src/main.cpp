b3fc0c5e1721b7376f90a2378d6a210df1fd7cf5
Volodymyr Hut <volodymyr.hut@gmail.com>
2023-09-07 20:16:00 +0300

    refactor

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,17 @@
 #include "gate_server/GateServer.h"
 #include "settings/Settings.h"
 
-GateLeaf    left(D0, D1);
-GateLeaf    right(D2, D3);
-Gate        gate(left, right, 1000);
+GateLeaf    left;
+GateLeaf    right;
+Gate        gate(left, right);
 GateServer  server(80, gate);
 
 void setup() {
     Serial.begin(9600);
     LittleFS.begin();
-
+    analogWriteFreq(5000);
+    left.configureGateLeaf(Settings::readSettings(), "left");
+    right.configureGateLeaf(Settings::readSettings(), "right");
     gate.initPins();
     Network::connectToWifi("TP-LINK_EFDA52", "kul1551luk");
     server.start();
